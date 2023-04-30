34e8c39dbed7b956bca67cca18e58e69aaca0ef8
NK <oscarfu0721@gmail.com>
2023-04-30 19:25:29 +0100

    fix

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+const config: ConfigURLS = {
+  api: "https://api.night0721.me",
+  code: "https://cdn.night0721.me/api/paste",
+  url: "https://cdn.night0721.me/api/url",
+};
+
+interface ConfigURLS {
+  api: string;
+  code: string;
+  url: string;
+}
+export { config, ConfigURLS };
--- src/index.ts
+++ src/index.ts
@@ -52,17 +52,4 @@ export {
 export { getLilaseDownloads } from "./functions/getLilaseDownloads";
 export { emojify } from "./functions/emojify";
 export { obama } from "./functions/obama";
-
-const config: ConfigURLS = {
-  api: "https://api.night0721.me",
-  code: "https://cdn.night0721.me/api/paste",
-  url: "https://cdn.night0721.me/api/url",
-};
-
-interface ConfigURLS {
-  api: string;
-  code: string;
-  url: string;
-}
-
-export { config };
+export { ConfigURLS, config } from "./config";
